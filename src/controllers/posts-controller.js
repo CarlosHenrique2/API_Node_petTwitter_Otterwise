aca96f78f91d431041411f6708d874141c8eeb11
Carlos Henrique <carlos.eletronic@hotmai.com>
2022-04-04 23:32:53 -0300

    fix errors posts

--- src/controllers/posts-controller.js
+++ src/controllers/posts-controller.js
@@ -40,11 +40,26 @@ export const posts = async (req, res) => {
   }
 };
 
+export const getallPosts = async (req, res) => {
+  const { text, authorId } = req.body;
+  try {
+    const user = await prisma.user.findMany({
+      where: {
+        id: authorId,
+      },
+    });
+    return res.send(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: `Cannot fetch posts` });
+  }
+};
+
 export const index = async (req, res) => {
-  const page = req.page;
+  const page = req.page - 1;
   try {
     const results = await prisma.post.findMany({
-      skip: 10,
+      skip: page * 10,
       take: 10,
     });
     return res.send(results);
